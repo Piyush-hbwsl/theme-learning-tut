29b2c1731ce3b594b2865d8d9dcf3b69a121b370
Piyush Agarwal <piyush.agarwal@hbwsl.com>
2023-03-06 12:55:25 +0530

    made changes in file

--- functions.php
+++ functions.php
@@ -122,13 +122,13 @@ add_action( 'after_setup_theme', '_displayfly_content_width', 0 );
 function _displayfly_widgets_init() {
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', '_displayfly' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', '_displayfly' ),
+			'name' => esc_html__('Sidebar', 'wp-theme'),
+			'id' => 'sidebar-1',
+			'description' => esc_html__('Add widgets here.', 'wp-theme'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
+			'after_widget' => '</section>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>',
 		)
 	);
 }
